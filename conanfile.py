22af4c019b2114ec805b87fb3fa173f5fbab5ab2
Edgar <Edgar@AnotherFoxGuy.com>
2018-09-17 15:25:31 +0200

    Upgrade to OGRE 1.11.2

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans.tools import os_info, SystemPackageTool
 
 class MyGUIConan(ConanFile):
     name = "MyGUI"
-    version = "3.2.3-OGRE-1.9.1"
+    version = "3.2.3-OGRE-1.11.2"
     license = "MIT"
     url = "https://github.com/AnotherFoxGuy/conan-MyGUI"
     description = "Fast, flexible and simple GUI."
@@ -12,11 +12,11 @@ class MyGUIConan(ConanFile):
 
     def requirements(self):
         self.requires.add('OGREdeps/2018-07@anotherfoxguy/stable')
-        self.requires.add('OGRE/1.9.1@anotherfoxguy/stable')
+        self.requires.add('OGRE/1.11.2@anotherfoxguy/stable')
 
     def source(self):
         self.run("git clone --depth 1 https://github.com/MyGUI/mygui.git . ")
-        tools.download("https://raw.githubusercontent.com/OGRECave/ogre/v1.9.1/CMake/Packages/FindOGRE.cmake", "FindOGRE.cmake")
+        tools.replace_in_file("CMake/InstallResources.cmake", "if (MYGUI_RENDERSYSTEM EQUAL 3)", "if (FALSE)")
         tools.replace_in_file("CMakeLists.txt", "set(CMAKE_MODULE_PATH", "set(CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR}")
         tools.replace_in_file("CMakeLists.txt", "# MYGUI BUILD SYSTEM", "include(conan_paths.cmake)")
 
