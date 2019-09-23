94dd5ece89efb7fc55b8326d61d2fd6cc4dea4f1
Edgar <Edgar@AnotherFoxGuy.com>
2019-09-23 18:55:33 +0200

    :zap: Replaced OGREdeps with Conan packages

--- conanfile.py
+++ conanfile.py
@@ -1,28 +1,38 @@
 from conans import ConanFile, CMake, tools
-from conans.tools import os_info, SystemPackageTool
+from conans.tools import os_info
+
 
 class MyGUIConan(ConanFile):
     name = "MyGUI"
-    version = "3.2.3-OGRE-1.11.6-with-patches"
+    version = "3.2.3"
     license = "MIT"
     url = "https://github.com/AnotherFoxGuy/conan-MyGUI"
     description = "Fast, flexible and simple GUI."
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake_paths"
+    generators = "cmake"
 
     def requirements(self):
-        self.requires.add('OGRE/1.11.6-with-patches@anotherfoxguy/stable')
+        self.requires.add('OGRE/[1.x]@anotherfoxguy/stable')
         if os_info.is_windows:
-            self.requires.add('OGREdeps/[20.x]@anotherfoxguy/stable')
+            self.requires.add('zlib/[1.x]@conan/stable')
+            self.requires.add('freetype/[2.x]@bincrafters/stable')
 
     def source(self):
         git = tools.Git()
         git.clone("https://github.com/MyGUI/mygui.git")
-        #git.checkout("07da0620e0d835346452b02b7b1ba7c583429736")
+        git.checkout("0726ed4ae70b3479677a794a6a3dd5d6cc48ed62")
+
         tools.replace_in_file("CMake/InstallResources.cmake", "if (MYGUI_RENDERSYSTEM EQUAL 3)", "if (FALSE)")
+
+        tools.replace_in_file("CMakeLists.txt", "# Find dependencies",
+                              "find_library(ZLIB_LIBRARY NAMES zlib zlib_d PATH_SUFFIXES lib)")
+
         tools.replace_in_file("CMakeLists.txt", "set(CMAKE_MODULE_PATH", "set(CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR}")
-        tools.replace_in_file("CMakeLists.txt", "# MYGUI BUILD SYSTEM", "include(conan_paths.cmake)")
-        tools.replace_in_file("CMakeLists.txt", "# Set up the basic build environment", "find_library(ZLIB_LIBRARY NAMES zlib zlib_d PATH_SUFFIXES lib)")
+
+        if os_info.is_windows:
+            tools.replace_in_file("CMakeLists.txt", "# Set up the basic build environment", '''  
+                                    include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+                                    conan_basic_setup(TARGETS)''')
 
     def build(self):
         cmake = CMake(self)
@@ -40,5 +50,5 @@ class MyGUIConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.includedirs = ['include/MYGUI']
-        self.cpp_info.libdirs = ['lib', 'lib/release', 'lib/debug']	 # Directories where libraries can be found
+        self.cpp_info.libdirs = ['lib', 'lib/release', 'lib/debug']  # Directories where libraries can be found
         self.cpp_info.libs = tools.collect_libs(self)
